94c190faec68ce515549d9c360d07fabe689e3a5
Armel Chesnais <armel.chesnais@gmail.com>
2017-05-08 23:00:49 +0000

    added downloadImageByUrl function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,11 @@
 var request = require('request');
+var fs = require('fs');
 require('dotenv').config();
+
+var downloadPath = "./avatars/"
+fs.mkdir(downloadPath);
+
+
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -25,19 +31,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
          })
          .on("end", function() {
            contributorList = JSON.parse(result);
-           getAvatarLinks(contributorList, console.log);
+           for (var i = 0; i < contributorList.length; i++) {
+             var avatarUrl = contributorList[i]["avatar_url"];
+             var filePath =  downloadPath + contributorList[i]["login"] + ".jpg"
+             cb(avatarUrl, filePath);
+           };
          });
   // console.log(requestURL);
 }
 
-function getAvatarLinks(list, callback) {
-  for (var i = 0; i < list.length; i++) {
-    callback(list[i]["avatar_url"]);
-  }
+
+
+function downloadImageByUrl(url, filePath) {
+  request.get( url )
+         .on("error", function(err) {
+          throw err;
+         })
+         .pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function() {
+  console.log(arguments[0], arguments[1]);
 });
+
+downloadImageByUrl("https://avatars3.githubusercontent.com/u/1615?v=3", "./avatars/jeresig.jpg")
 
