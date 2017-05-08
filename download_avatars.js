a6f87560f4d12f6f4fa8bf89a775a4afb54aa6d3
Armel Chesnais <armel.chesnais@gmail.com>
2017-05-08 22:33:07 +0000

    implemented get request

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,25 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ process.env.GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  console.log(requestURL);
+  var options = {
+                  url: requestURL,
+                  headers: {
+                    'User-Agent': "ArmelChesnais' download avatars app"
+                  }
+  }
+
+  var result = "";
+  request.get( options )
+         .on("error", function(err) {
+          throw err;
+         })
+         .on("data", function(data) {
+           result += data;
+         })
+         .on("end", function() {
+           console.log(result);
+         });
+  // console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
