ff9dfde620a0186a43d4edf3d81c5f38196d2e54
Armel Chesnais <armel.chesnais@gmail.com>
2017-05-09 14:12:23 +0000

    added handling for incorrect owner/repo

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,14 @@ var request = require('request');
 var fs = require('fs');
 require('dotenv').config();
 
-var downloadPath = "./avatars/"
+var downloadPath = "./avatars/" // set default download folder location
 
-
+// make a get request to obtain list of avatar links, and applies callback function to each.
 function getRepoContributors(repoOwner, repoName, cb) {
+  // prepare request URL. Uses .env info to avoid revealing API key.
   var requestURL = 'https://'+ process.env.GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+  // sets options for get request, so header is provided.
   var options = {
                   url: requestURL,
                   headers: {
@@ -15,26 +17,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
                   }
   }
 
-  var result = "";
+  var result = ""; // default output from get request is empty.
   var contributorList;
   request.get( options )
          .on("error", function(err) {
           throw err;
          })
          .on("data", function(data) {
-           result += data;
+           result += data; // append each chunk of data to output.
          })
          .on("end", function() {
+           // Once complete, parse all chunks into JSON object
            contributorList = JSON.parse(result);
-           for (var i = 0; i < contributorList.length; i++) {
-             var avatarUrl = contributorList[i]["avatar_url"];
-             var filePath =  downloadPath + contributorList[i]["login"] + ".jpg"
-             cb(avatarUrl, filePath);
-           };
+           processAndCallback(contributorList, cb);
          });
   // console.log(requestURL);
 }
 
+function processAndCallback(data, cb) {
+  if ( data.message && data.message.toLowerCase() === "not found") {
+    console.log( "Repo or owner incorrect" );
+  } else {
+    // iterate through object to pull avatar URL and set individual download path as per user's login name
+    for (var i = 0; i < data.length; i++) {
+     var avatarUrl = data[i]["avatar_url"];
+     var filePath =  downloadPath + data[i]["login"] + ".jpg"
+     cb(avatarUrl, filePath); // callback invoked to download each individual image.
+    }
+  }
+}
 
 
 function downloadImageByUrl(url, filePath) {
@@ -43,29 +54,29 @@ function downloadImageByUrl(url, filePath) {
           throw err;
          })
          .on("end", function() {
+           // output message to notify user when each individual file is done.
            console.log(filePath, "download complete");
          })
-         .pipe(fs.createWriteStream(filePath))
-         ;
+         .pipe(fs.createWriteStream(filePath)); // pipe image data to the prior-defined file path.
 }
 
 function processInput(inputs) {
-  if ( inputs.length < 2 ) {
+  if ( inputs.length !== 2 ) { // rejects inputs if there are fewer than 2 arguments
     console.log("Please provide two arguments: <owner> <repo>");
   }
   else {
+    // if there are at least 2 argument, first ensure download folder exists.
     if (!fs.existsSync(downloadPath) ) {
-      fs.mkdir(downloadPath);
+      fs.mkdir(downloadPath); // create if not.
     }
 
     console.log('Welcome to the GitHub Avatar Downloader!');
 
+    // start processing the data.
     getRepoContributors(inputs[0], inputs[1], downloadImageByUrl);
   }
 }
 
 processInput( process.argv.slice(2) );
-// console.log(process.argv.slice(2));
-
 
 
